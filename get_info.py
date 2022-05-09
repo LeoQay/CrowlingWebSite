45ad02268876e5266971f1f41b36f5142ad2fab7
LeoQay <reliativism@yandex.ru>
2022-05-09 14:25:39 +0300

    get info

--- get_info.py
+++ get_info.py
@@ -6,6 +6,20 @@ import urllib.request as request
 import collections as col
 
 
+def namer(func, name):
+    def wrapper(thing):
+        return func(thing, name)
+    return wrapper
+
+
+def has_attr(tag, name):
+    return tag.has_attr(name)
+
+
+def consist(name_, name):
+    return name_.find(name) != -1
+
+
 def get_tag(start_tag, path):
     tag = start_tag
     for args, kwargs, pos in path:
@@ -16,45 +30,104 @@ def get_tag(start_tag, path):
     return tag
 
 
-def get_name(soup):
+def get_name(stat, soup):
     path = [
-        (('div',), {'class_': 'main-content'}, -1),
-        (('div',), {'id': 'page'}, -1),
         (('div',), {'class_': 'breadcrumbs-widget pjax-inner-replace'}, -1),
         (('div',), {'class_': 'container'}, -1),
         (('ul',), {'class_': 'breadcrumbs-list'}, -1),
         (('li',), {}, 3),
         (('span',), {}, -1)
     ]
-    dest = get_tag(soup.html.body, path)
-    return dest.get_text()
+    dest = get_tag(soup, path)
+    stat['name'] = dest.get_text()
 
 
-def get_image(soup):
+def get_image(stat, soup):
     path = [
-        (('div',), {'class_': 'main-content'}, -1),
-        (('div',), {'id': 'page'}, -1),
-        (('div',), {'class_': 'game-card'}, -1),
-        (('div',), {'class_': 'game-info-image'}, -1),
         (('img',), {}, -1),
     ]
-    tag = get_tag(soup.html.body, path)
-    return tag['src']
+    tag = get_tag(soup, path)
+    stat['image'] = tag['src']
 
 
-def get_market_url(soup):
+def get_market_url(stat, soup):
     path = [
-        (('div',), {'class_': 'main-content'}, -1),
-        (('div',), {'id': 'page'}, -1),
-        (('div',), {'class_': 'game-card'}, -1),
-        (('div',), {'class_': 'game-info-image'}, -1),
         (('a',), {'class_': 'game-link-widget'}, -1)
     ]
-    tag = get_tag(soup.html.body, path)
+    tag = get_tag(soup, path)
     url = tag['href']
     req = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     res = urllib.request.urlopen(req)
-    return res.geturl()
+    stat['market_url'] = res.geturl()
+
+
+def get_from_game_info_image(stat, soup):
+    path = [
+        (('div',), {'class_': 'game-info-image'}, -1),
+    ]
+    soup = get_tag(soup, path)
+    get_image(stat, soup)
+    get_market_url(stat, soup)
+
+
+def get_wishlist_counter(stat, soup):
+    path = [
+        ((), {'class_': namer(consist, 'wishlisted-game')}, -1),
+        (('span',), {'class_': 'user-count'}, -1),
+        (('span',), {'class_': 'count'}, -1)
+    ]
+    soup = get_tag(soup, path)
+    stat['wishlist_count'] = soup.get_text()
+
+
+def get_alert_counter(stat, soup):
+    path = [
+        ((), {'class_': namer(consist, 'alerted-game')}, -1),
+        (('span',), {'class_': 'user-count'}, -1),
+        (('span',), {'class_': 'count'}, -1)
+    ]
+    soup = get_tag(soup, path)
+    stat['alert_count'] = soup.get_text()
+
+
+def get_own_counter(stat, soup):
+    path = [
+        ((), {'class_': namer(consist, 'owned-game')}, -1),
+        (('span',), {'class_': 'user-count'}, -1),
+        (('span',), {'class_': 'count'}, -1)
+    ]
+    soup = get_tag(soup, path)
+    stat['owners_count'] = soup.get_text()
+
+
+def get_from_game_collection_actions(stat, soup):
+    path = [
+        (('div',), {'class_': 'game-header game-header-container container'}, -1),
+        ((namer(has_attr, 'data-counters-url'),), {}, -1),
+    ]
+    soup = get_tag(soup, path)
+    get_wishlist_counter(stat, soup)
+    get_alert_counter(stat, soup)
+    get_own_counter(stat, soup)
+
+
+def get_from_game_card(stat, soup):
+    path = [
+        (('div',), {'class_': 'game-card'}, -1),
+    ]
+    soup = get_tag(soup, path)
+    get_from_game_info_image(stat, soup)
+    get_from_game_collection_actions(stat, soup)
+
+
+def get_from_main_content_page(stat, soup):
+    path = [
+        (('div',), {'class_': 'main-content'}, -1),
+        (('div',), {'id': 'page'}, -1),
+    ]
+    soup = get_tag(soup, path)
+    get_name(stat, soup)
+    get_from_game_card(stat, soup)
 
 
 def collect_stat(url):
@@ -64,10 +137,10 @@ def collect_stat(url):
     with open('index.html', 'r') as file:
         soup = bs4.BeautifulSoup(file, 'html.parser')
 
+    soup = soup.html.body
+
     stat['url'] = url
-    stat['name'] = get_name(soup)
-    stat['image'] = get_image(soup)
-    stat['market_url'] = get_market_url(soup)
+    get_from_main_content_page(stat, soup)
 
     return stat
 
